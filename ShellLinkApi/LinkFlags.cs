160272af670631ccc0d677348e2a17ab552227bf
John Tasler <taz@mysticpop.com>
2017-08-13 18:38:50 -0700

    Moved LinkFlags out of the Structures folder. Removed experimental StringData class.

--- ShellLinkApi/LinkFlags.cs
+++ ShellLinkApi/LinkFlags.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Runtime.InteropServices;
 
-namespace ShellLinkApi.Structures
+namespace ShellLinkApi
 {
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public struct LinkFlags
